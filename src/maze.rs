c827cbf7679febd6c9cbce6add35bed5a5e078f3
Patrick Lambein <patrick@lambein.name>
2014-08-25 23:15:47 +0100

    Add path generating algorithm.

--- src/maze.rs
+++ src/maze.rs
@@ -1,24 +1,26 @@
-use std::rand;
-
-static grid_size : uint = 4;
+#[allow(dead_code)]
+static grid_size : uint = 9;
 
 type Coord = (uint, uint);
 
-#[deriving(Rand)]
+#[deriving(PartialEq,Rand)]
 enum Color {
     White
 }
 
-enum Cell {
+#[deriving(PartialEq)]
+enum CellType {
     Wall,
     Path(Color),
 }
 
+#[allow(dead_code)]
 struct Grid {
-    cells : [[Cell, ..grid_size], ..grid_size],
-    path_in_construction : Vec<Coord>,
+    cells : [[CellType, ..grid_size], ..grid_size],
+    path_in_construction : Vec<(Coord, Vec<Coord>)>,
 }
 
+#[allow(dead_code)]
 impl Grid {
 
     fn new() -> Grid {
@@ -28,66 +30,149 @@ impl Grid {
         }
     }
 
-    fn collect_neighbours (&mut self, x : uint, y : uint)
-                             -> Vec<(Coord, Cell)> {
+    fn at (&mut self, coords : Coord) -> CellType {
+        match coords {
+            (x, y) => self.cells[x][y]
+        }
+    }
+
+    fn collect_neighbours (&mut self, coords : Coord)
+                             -> Vec<(Coord)> {
         let mut neighbours = Vec::with_capacity(4);
 
+        let (x, y) = coords;
+
         if x > 1 {
-            neighbours.push(((x-1, y), self.cells[x-1][y]));
+            neighbours.push((x-1, y));
         }
         if y > 1 {
-            neighbours.push(((x, y-1), self.cells[x][y-1]));
+            neighbours.push((x, y-1));
         }
-        if x < grid_size - 1 {
-            neighbours.push(((x+1, y), self.cells[x+1][y]));
+        if x + 1 < grid_size {
+            neighbours.push((x+1, y));
         }
-        if y < grid_size - 1 {
-            neighbours.push(((x, y+1), self.cells[x][y+1]));
+        if y + 1 < grid_size {
+            neighbours.push((x, y+1));
         }
 
         return neighbours;
     }
 
-    fn near_maze (&mut self, x : uint, y : uint) -> bool {
-        let neighbours = self.collect_neighbours(x, y);
+    fn near_maze (&mut self, coords : Coord) -> bool {
+        let neighbours = self.collect_neighbours(coords);
 
-        for neigh in neighbours.iter() {
-            match *neigh {
-                (_, Path(_)) => return true,
-                _            => continue,
+        for &cs in neighbours.iter() {
+            match self.at(cs) {
+                Path(_) => return true,
+                _       => continue,
             }
         }
 
         return false;
     }
 
-    fn near_path (&mut self, x : uint, y : uint) -> bool {
-        let neighbours = self.collect_neighbours(x, y);
+    fn in_path (&mut self, coords : Coord) -> bool {
+        for cell in self.path_in_construction.iter() {
+            match *cell {
+                (cs, _) if cs == coords => return true,
+                _   => continue
+            }
+        }
+
+        return false;
+    }
+
+    fn near_path (&mut self, coords : Coord) -> bool {
+        let neighbours = self.collect_neighbours(coords);
 
         let mut count : int = 0;
 
-        for neigh in neighbours.iter() {
-            match *neigh {
-                (cs, _) => if self.path_in_construction.contains(&cs) {
+        for &cs in neighbours.iter() {
+            if self.in_path(cs) {
                 count += 1;
-                }
             }
         }
 
-        return (count > 1);
+        return count > 1;
     }
 
-    fn commit_path (&mut self) {
-        let color : Color = std::rand::random();
+    fn explorable_neighbours (&mut self, coords : Coord)
+                                     -> Vec<Coord> {
+        let neighbours = self.collect_neighbours(coords);
+        let mut explorable = Vec::with_capacity(4);
 
-        for cell in self.path_in_construction.iter() {
-            match *cell {
-                (x, y) => self.cells[x][y] = Path(color)
+        for &cs in neighbours.iter() {
+            if self.at(cs) == Wall || self.near_path(cs) {
+                continue;
             }
+
+            explorable.push(cs);
         }
 
-        self.path_in_construction.clear();
+        return explorable;
     }
+
+    fn select_at_random<T> (leads : &mut Vec<T>) -> Option<T> {
+        leads.pop() // TODO: do something more random.
+    }
+
+    fn add_cell_to_path(&mut self, coords : Coord) {
+        let ns = self.explorable_neighbours(coords);
+
+        self.path_in_construction.push((coords, ns));
+    }
+
+    fn extend_path (&mut self) -> bool {
+        let option_new_cell =
+            Grid::select_at_random(self.path_in_construction
+                                       .get_mut(0)
+                                       .mut1());
+
+        match option_new_cell {
+            Some(cs) => self.add_cell_to_path(cs),
+            None        => return false
+        }
+
+        return true;
+    }
+
+    fn new_path_origin (&mut self, coords : Coord) {
+        let neighbours = self.collect_neighbours(coords);
+
+        self.path_in_construction.push((coords, neighbours));
+    }
+
+    fn backtrack (&mut self) {
+        self.path_in_construction.pop();
+    }
+
+    fn new_path (&mut self, coords : Coord) {
+        self.new_path_origin(coords);
+
+        loop {
+            let cs = *(self.path_in_construction[0].ref0());
+
+            if self.near_maze(cs) {
+                break;
+            }
+
+            if !self.extend_path() {
+                self.backtrack();
+            }
+        }
+    }
+
+//    fn commit_path (&mut self) {
+//        let color : Color = std::rand::random();
+//
+//        for cell in self.path_in_construction.iter() {
+//            match *cell {
+//                (x, y) => self.cells[x][y] = Path(color)
+//            }
+//        }
+//
+//        self.path_in_construction.clear();
+//    }
 
 }
 
